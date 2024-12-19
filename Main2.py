3c08a1acb1fca797fba0b018b8be21fc67a7b18d
1404-694 <1404@pacificworldschool.com>
2024-12-19 19:00:44 +0530

    Add files via upload

---
+++ Main2.py
@@ -0,0 +1,9 @@
+def weather_codition():
+    print("The weather is pleasent in: ",spring)
+    print("The weather is same in: ",autmun)
+
+spring="autmun"
+autmun= "Winter"
+
+weather_codition()
+    
